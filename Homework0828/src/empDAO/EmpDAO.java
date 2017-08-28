5cd2a293c9de2035266eb6596299ddf982811f97
Ross.zhang <yuefeiduzi@foxmail.com>
2017-08-28 22:02:40 +0800

    Mytext update@2017年8月16日11:22:48 update@2017年8月16日11:45:58 update@2017年8月16日13:53:52 update@2017年8月16日14:50:10 大体实现 @2017年8月16日15:08:53 ver2代码 小改 @2017年8月16日16:19:52 @2017年8月16日19:45:56 删除部分项目 @2017年8月17日9:31:36 窗口 @2017年8月28日19:16:09 大体完成，补部门 @2017年8月28日21:44:00

--- Homework0828/src/empDAO/EmpDAO.java
+++ Homework0828/src/empDAO/EmpDAO.java
@@ -7,8 +7,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EmpDAO {
 
@@ -84,7 +82,7 @@ public class EmpDAO {
         }
         return row;
     }
-
+}
 //    public List<String> getDepart() {
 //        List<String> departmentName = new ArrayList<>();
 //        return
