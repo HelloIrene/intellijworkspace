7ea0144d9942a7480cc96069167ff91ab0f51029
Ross.zhang <yuefeiduzi@foxmail.com>
2017-08-28 19:16:44 +0800

    Mytext update@2017年8月16日11:22:48 update@2017年8月16日11:45:58 update@2017年8月16日13:53:52 update@2017年8月16日14:50:10 大体实现 @2017年8月16日15:08:53 ver2代码 小改 @2017年8月16日16:19:52 @2017年8月16日19:45:56 删除部分项目 @2017年8月17日9:31:36 窗口 @2017年8月28日19:16:09

---
+++ Homework0828/src/T_User.java
@@ -0,0 +1,82 @@
+import java.sql.Date;
+
+public class T_User {
+    public int empNo;
+    public String empName;
+    public String empJob;
+    public Date empDate;
+    public double empSal;
+    public double empComm;
+    public String empDepart;
+
+    public T_User() {
+
+    }
+
+    public T_User(int empNo, String empName, String empJob, Date empDate,
+                  double empSal, double empComm, String empDepart) {
+        this.empNo = empNo;
+        this.empName = empName;
+        this.empJob = empJob;
+        this.empDate = empDate;
+        this.empSal = empSal;
+        this.empComm = empComm;
+        this.empDepart = empDepart;
+    }
+
+    public int getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(int empNo) {
+        this.empNo = empNo;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpJob() {
+        return empJob;
+    }
+
+    public void setEmpJob(String empJob) {
+        this.empJob = empJob;
+    }
+
+    public Date getEmpDate() {
+        return empDate;
+    }
+
+    public void setEmpDate(Date empDate) {
+        this.empDate = empDate;
+    }
+
+    public double getEmpSal() {
+        return empSal;
+    }
+
+    public void setEmpSal(double empSal) {
+        this.empSal = empSal;
+    }
+
+    public double getEmpComm() {
+        return empComm;
+    }
+
+    public void setEmpComm(double empComm) {
+        this.empComm = empComm;
+    }
+
+    public String getEmpDepart() {
+        return empDepart;
+    }
+
+    public void setEmpDepart(String empDepart) {
+        this.empDepart = empDepart;
+    }
+}
